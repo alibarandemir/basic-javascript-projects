b83065e449ced8a07e708ce2ad857a8bb97b2c7a
Ali Baran Demir <alibarandemir798@gmail.com>
2023-03-10 17:52:15 +0300

    sign app added

---
+++ signInApp/script.js
@@ -0,0 +1,27 @@
+//import { passwordStrength } from './node_modules/check-password-strength/dist/umd';
+//select dom elements
+var eyeIcon = document.getElementById("eyeIcon");
+var passwordInput = document.getElementById("password");
+
+eyeIcon.style.cursor="pointer";
+
+eyeIcon.addEventListener("click",changeIcon);
+function changeIcon(){
+    eyeIcon.classList.forEach((item)=>{
+        eyeIcon.classList.remove(item);    
+    })
+    if(passwordInput.type === "password"){
+        passwordInput.type="text";
+        eyeIcon.classList.add("fa-solid");
+        eyeIcon.classList.add("fa-eye-slash");
+    }
+    else{
+        passwordInput.type="password";
+        eyeIcon.classList.add("fa-sharp");
+        eyeIcon.classList.add("fa-solid");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
+
+
+console.log(passwordStrength("ASDDassd").value);
