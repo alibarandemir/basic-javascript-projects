7ff07ed050509d176a82b6b6d9150718a2dce228
Ali Baran Demir <alibarandemir798@gmail.com>
2023-03-27 00:10:03 +0300

    signInAppPasswordChecking

--- signInApp/script.js
+++ signInApp/script.js
@@ -1,7 +1,8 @@
 //select dom elements
 var eyeIcon = document.getElementById("eyeIcon");
 var passwordInput = document.getElementById("password");
-
+var passwordDom = document.getElementsByClassName("passwordStrength");
+var signInBtn = document.getElementById("btn");
 eyeIcon.style.cursor="pointer";
 
 eyeIcon.addEventListener("click",changeIcon);
@@ -22,5 +23,62 @@ function changeIcon(){
     }
 }
 
+//checking password strength
+var passwordObject = {
+    value:"",
+    contains:[],
+    lengt:"",
+    score:0,
+}
+const checkPassword = (password)=>{
+    passwordObject.length=password.length;
+    console.log(password);
+    if(password !== password.toLowerCase()){
+        passwordObject.score+=10;
+        passwordObject.contains.push("upperLetter");
+    }
+    if(password.length>6){
+        passwordObject.score+=10;
+        passwordObject.contains.push("minSixLetter");
+    }
+    if(isIncludeNumber(password)){
+        passwordObject.score+=10;
+        passwordObject.contains.push("includeNumber");
+    }
+    
+    function isIncludeNumber(string){
+        function containsNumber(string) {
+            return /[0-9]/.test(string);
+          }
+        if(containsNumber(string)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    console.log(`Password score: ${passwordObject.score}`);
+    if(passwordObject.score===30){
+        passwordObject.value= "Strong";
+        passwordDom[0].innerHTML = `Your password is ${passwordObject.value}`;
+    }
+    if(passwordObject.score===20){
+        passwordObject.value = "Medium";
+        passwordDom[0].innerHTML= `Your password is ${passwordObject.value}`;
+    }
+    if(passwordObject.score===10){
+        passwordObject.value= "Weak";
+        passwordDom[0].innerHTML = `Your password is ${passwordObject.value}`;
+    }
+    if(passwordObject.score===0){
+        passwordObject.value = "Too Weak";
+        passwordDom[0].innerHTML = `Your password is ${passwordObject.value}`;
+    }
+    
+}
+signInBtn.addEventListener("click",function(){
+    checkPassword(passwordInput.value);
+});
 
 
